ad9e5e4cbe8bd603c67bf627601ab1a2e619b427
Lital Shahar <litalshahar@gmail.com>
2026-04-19 16:09:28 +0300

    עדכון קישור דף נחיתה

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ function randomReply() {
 }
 
 // הודעת DM
-const LANDING_PAGE = '[קישור לדף הנחיתה]'; // ← תתעדכן בקרוב
+const LANDING_PAGE = 'https://lrs.ravpage.co.il/%D7%A4%D7%A8%D7%98%D7%99%D7%9D%20%D7%A9%D7%99%D7%97%D7%AA%20%D7%94%D7%AA%D7%90%D7%9E%D7%94';
 
 const DM_TEXT = `הי, כאן ליטל שחר ❤️
 
